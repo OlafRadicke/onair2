20f992bda77a2f9a30a63e7dbb6e4cd96764ae04
Olaf Radicke <briefkasten@olaf-radicke.de>
2014-06-20 15:33:21 +0200

    extracting station name and minits

--- README.md
+++ README.md
@@ -5,3 +5,66 @@ MVV
 ===
 http://www.mvg-live.de/ims/dfiStaticAuswahl.svc?haltestelle=Feldmoching+Bf.&sbahn=checked
 https://github.com/tautologistics/node-htmlparser
+
+---> rowEven:
+[
+  {
+    "type":"tag",
+    "name":"tr",
+    "attribs":
+    {
+      "class":"rowEven"
+    },
+    "children":
+      [
+        {
+          "data":"\r\n\t\t\t\t","type":"text"
+        },
+        {
+          "type":"tag",
+          "name":"td",
+          "attribs":
+          {
+            "class":"lineColumn"
+          },
+          "children":
+          [
+            {"data":"S1","type":"text"}
+          ]
+        },
+        {"data":"\t\t\t\r\n\t\t\t\t","type":"text"},
+        {
+          "type":"tag",
+          "name":"td",
+          "attribs":{"class":"stationColumn"},
+          "children":
+          [
+            {"data":"\r\n\t\t\t\t\tFreising\r\n\t\t\t\t\t","type":"text"},
+            {
+              "type":"tag",
+              "name":"span",
+              "attribs":{"class":"spacer"},
+              "children":
+              [
+                {"data":"&nbsp;","type":"text"}
+              ]
+            },
+            {"data":"\r\n\t\t\t\t","type":"text"}]},
+            {"data":"\t\t\t\t\r\n\t\t\t\t","type":"text"},
+            {
+              "type":"tag","name":"td",
+              "attribs":{"class":"inMinColumn"},
+              "children":
+              [
+                {"data":"16","type":"text"}
+              ]
+            },
+            {"data":"\r\n\t\t\t\t\r\n\t\t\t","type":"text"}
+          ]
+        }
+      ]
+
+["children"][1]["children"][1].["data"] = "\r\n\t\t\t\t\tFreising\r\n\t\t\t\t\t"
+---> rowOdd: []
+
+7 Min.
--- src/app/onair2.js
+++ src/app/onair2.js
@@ -2,6 +2,8 @@
 var execSync = require('exec-sync');
 var fs = require('fs');
 var htmlparser = require("htmlparser");
+var http = require('http');
+
 
 function OnAir2( initTimeStamp ) {
   this.lastupdate = initTimeStamp;
@@ -19,33 +21,85 @@ OnAir2.prototype.getStatus = function (req, res) {
   // test
   var asterisk_command = "echo \"SIP/ingrid\nSIP/pascal\nSIP/1240\"";
   var now_checktime = new Date().getTime();
+  var timeOut = 60;
 
-  if ( Math.round(( now_checktime - this.lastupdate) / 1000 ) > 60 ) {
+  if ( Math.round(( now_checktime - this.lastupdate) / 1000 ) > timeOut ) {
     this.lastupdate = now_checktime;
     console.log("update timestamp...");
   } else {
-    console.log("es sind noch nicht 60 Sek. vergangen: ");
+    console.log("es sind noch nicht " + timeOut + " Sek. vergangen: ");
     console.log( Math.round(( now_checktime - this.lastupdate) / 1000 ) );
   }
   console.log(this.lastupdate);
   console.log(now_checktime);
 
-  var activ_lines = execSync(asterisk_command);
-  console.log( "activ_lines:" + activ_lines);
-  var activ_lines_list = activ_lines.split("\n");
-  console.log("activ_lines_list: " + activ_lines_list);
-  for(var roomIndex in this.allstate.room ) {
-    for(var person in this.allstate.room[roomIndex] ) {
-      for( var phonCode in activ_lines_list) {
-        if( activ_lines_list[phonCode] === this.allstate.room[roomIndex][person].starfacecode ) {
-          this.allstate.room[roomIndex][person].line = "ON AIR";
-          break;
-        } else {
-          this.allstate.room[roomIndex][person].line = "OFF AIR";
+
+  if ( Math.round(( now_checktime - this.lastupdate) / 1000 ) > timeOut ) {
+    var activ_lines = execSync(asterisk_command);
+    console.log( "activ_lines:" + activ_lines);
+    var activ_lines_list = activ_lines.split("\n");
+    console.log("activ_lines_list: " + activ_lines_list);
+    for(var roomIndex in this.allstate.room ) {
+      for(var person in this.allstate.room[roomIndex] ) {
+        for( var phonCode in activ_lines_list) {
+          if( activ_lines_list[phonCode] === this.allstate.room[roomIndex][person].starfacecode ) {
+            this.allstate.room[roomIndex][person].line = "ON AIR";
+            break;
+          } else {
+            this.allstate.room[roomIndex][person].line = "OFF AIR";
+          }
         }
       }
     }
   }
+
+  // ########## train info #######################
+  var options = {
+    host: 'www.mvg-live.de',
+    port: 80,
+    path: '/ims/dfiStaticAuswahl.svc?haltestelle=Feldmoching+Bf.&sbahn=checkedm'
+  };
+
+  http.get(options, function(res) {
+    console.log("Got response: " + res.statusCode);
+    res.on('data', function (rawHtml) {
+      console.log('BODY: ' + rawHtml);
+
+      var anyRow = null;
+      var handler = new htmlparser.DefaultHandler(function(err, dom) {
+        if (err) {
+          sys.debug("Error: " + err);
+        } else {
+          var rowEven = htmlparser.DomUtils.getElements({ tag_name: "tr", class: "rowEven" }, dom);
+          if ( rowEven != null ) {
+//             console.log("---> rowEven: " + JSON.stringify(rowEven) );
+            var statonName = rowEven[0]["children"][3]["children"][0]["data"];
+            statonName = statonName.replace(/\t/g, '').replace(/\n/g, '')
+            console.log("statonName: " );
+            console.log( statonName );
+            console.log("---> rowEven::minuten: " + JSON.stringify(rowEven[0]["children"][5]["children"][0]["data"] ) );
+          }
+
+          var rowOdd = htmlparser.DomUtils.getElements({ tag_name: "tr", class: "rowOdd" }, dom);
+          if ( rowOdd != null ) {
+//             console.log("---> rowOdd: " + JSON.stringify(rowOdd) );
+            var statonName = rowOdd[0]["children"][3]["children"][0]["data"];
+            statonName = statonName.replace(/\t/g, '').replace(/\n/g, '')
+            console.log("statonName: " );
+            console.log( statonName );
+            console.log("---> rowOdd::minuten: " + JSON.stringify(rowOdd[0]["children"][5]["children"][0]["data"] ) );
+          }
+        }
+      }, { verbose: false });
+      var parser = new htmlparser.Parser(handler);
+      parser.parseComplete(rawHtml);
+
+
+    });
+
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+  });
 
 //   console.log("JSON.stringify(this.allstate):" + JSON.stringify(this.allstate));
   res.render('status', {allstate: this.allstate} );
