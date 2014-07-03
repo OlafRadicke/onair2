fc0540ed14f59b0ab2a84a6277c6e967f4e3e70a
Olaf Radicke <briefkasten@olaf-radicke.de>
2014-07-03 18:08:19 +0200

    admin controller is rady.

--- src/app/controls/admin.js
+++ src/app/controls/admin.js
@@ -4,36 +4,71 @@ var adminControllers;
 
 adminControllers = {
     'index': function (req, res) {
+        "use strict";
         var infotext = ""
         var stringStates = null;
+        var config = null;
         var statusFile = __dirname + '/../../var/status.json';
+        var configFile = __dirname + '/../../var/config.json';
+        // read config
         try {
-          stringStates = fs.readFileSync( statusFile,'utf8');
+            config = JSON.parse(fs.readFileSync( configFile,'utf8'));
+            console.log("JSON.stringify(config): " + JSON.stringify(config));
         } catch (e) {
-          console.log("Can't read: " + statusFile);
+            console.log("Can't read: " + configFile);
+            infotext = "Can't read: " + configFile;
+        }
+        // read status
+        try {
+            var stringStates = fs.readFileSync( statusFile, 'utf8' );
+        } catch (e) {
+            console.log("Can't read: " + statusFile + "\n" + e);
+            infotext = infotext + " Can't read: " + statusFile;
         }
 
         res.render('admin', {
             jsonDB: stringStates,
-            infoText: infotext
+            infoText: infotext,
+            config: config
         });
     },
     'updateStates': function (req, res) {
+        "use strict";
         var infotext = "";
-        var newStatus = req.body.newstatus;
         var statusFile = __dirname + '/../../var/status.json';
+        var configFile = __dirname + '/../../var/config.json';
+        var newStatus = req.body.newstatus;
+        var config = {
+            "asterisk_command": "",
+            "browser_interval": "",
+            "mvv_interval": "",
+            "asterisk_interval": ""
+        }
+        config.asterisk_command = req.body.asterisk_command;
+        config.browser_interval = req.body.browser_interval;
+        config.mvv_interval = req.body.mvv_interval;
+        config.asterisk_interval = req.body.asterisk_interval;
 
+        // write status
         try {
-            stringStates = fs.writeFileSync( statusFile, newStatus, 'utf8');
-            infotext = "Gespeichert!";
+            fs.writeFileSync( statusFile, newStatus, 'utf8');
+            infotext = "...Gespeichert!";
         } catch (e) {
-            console.log("Can't read: " + statusFile);
-            infotext = "Konte nicht gespeichert werden!";
+            infotext = statusFile + "konte nicht gespeichert werden! ";
+        }
+
+        // wite config...
+        try {
+            fs.writeFileSync( configFile, JSON.stringify(config), 'utf8');
+            infotext = "...Gespeichert!";
+        } catch (e) {
+            infotext = configFile + "konte nicht gespeichert werden! " + infotext;
         }
 
         res.render('admin', {
           jsonDB: newStatus,
-          infoText: infotext
+          infoText: infotext,
+          config: config
         });
     }
 };
